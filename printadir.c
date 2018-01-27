96a8b4c5e57ac57aad7197698ba15852662c0ec0
projectbcc <brendanclegg@yahoo.com>
2018-01-26 22:07:22 -0500

    Add files via upload

---
+++ printadir.c
@@ -0,0 +1,10 @@
+/* This program will print a directory name using escape characters to correctly display a directory.*/
+//By Brendan Clegg
+
+#include <stdio.h>
+
+main()
+
+{
+	printf("C:\\cygwin\\home\\administrator\n");
+}
