5891cd65e920c4c6fa83c2a80b69fdc02df4bb06
projectbcc <brendanclegg@yahoo.com>
2018-01-26 22:43:08 -0500

    Add files via upload

---
+++ calendar.c
@@ -0,0 +1,15 @@
+/* This is a program that prints the current month, which is January 2018. */
+// By Brendan Clegg
+
+#include <stdio.h>
+
+main()
+
+{
+	printf("Sun\tMon\tTue\tWed\tThu\tFri\tSat\n");
+	printf("\t1\t2\t3\t4\t5\t6\n");
+	printf("7\t8\t9\t10\t11\t12\t13\n");
+	printf("14\t15\t16\t17\t18\t19\t20\n");
+	printf("21\t22\t23\t24\t25\t26\t27\n");
+	printf("28\t29\t30\t31\n");
+}
