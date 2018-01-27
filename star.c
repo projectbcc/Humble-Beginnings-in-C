a96f38ac555055425b76bde0503627b80d8287c3
projectbcc <brendanclegg@yahoo.com>
2018-01-26 22:19:08 -0500

    Add files via upload

---
+++ star.c
@@ -0,0 +1,16 @@
+/* This is a program that prints a star using tab escape sequences. */
+// By Brendan Clegg
+
+#include <stdio.h>
+
+main()
+
+{
+	printf("\t\t\t*\n");
+	printf("\t\t*\t\t*\n");
+	printf("\t*\t\t\t\t*\n");
+	printf("*\t\t\t\t\t\t*\n");
+	printf("\t*\t\t\t\t*\n");
+	printf("\t\t*\t\t*\n");
+	printf("\t\t\t*\n");
+}
