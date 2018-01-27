4430f931e477821b72b7f1c7d2fa5daefd5cc88e
projectbcc <brendanclegg@yahoo.com>
2018-01-26 22:02:34 -0500

    Add files via upload

---
+++ favoritequote.c
@@ -0,0 +1,10 @@
+/* This is a program that prints my favorite quote with the quotation marks. */
+//By Brendan Clegg
+
+#include <stdio.h>
+
+main()
+
+{
+	printf("\"To be or not to be. That is the question.\"\n\t-Shakespeare.\n");
+}
