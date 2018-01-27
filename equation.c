06be566581edcc0a6fed1bdaa1a8d4e818b65c1a
projectbcc <brendanclegg@yahoo.com>
2018-01-27 12:17:03 -0500

    Add files via upload

---
+++ equation.c
@@ -0,0 +1,15 @@
+/* This is a program that takes a predetermined set of variables and displays the 
+output of the given equation. */
+// By Brendan Clegg
+
+#include <stdio.h>
+
+main()
+
+{
+	int a = 5;
+	int b = 1;
+	int x = 10;
+	int y = 5;
+	printf("%d\n", (a - b) * (x - y));
+}
